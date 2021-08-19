273220006b795e7ba2edf238b01e65c8806f9cfc
SAM <saiyedmustaq786@gmail.com>
2021-08-19 23:11:32 +0530

    Initial commit

---
+++ app/src/main/java/com/mustaq/sqlitecrud/StudentListActivity.kt
@@ -0,0 +1,12 @@
+package com.mustaq.sqlitecrud
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StudentListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_student_list)
+    }
+}
